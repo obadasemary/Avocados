74aa3413ebae4e4680741d481ee5f1544331c3bc
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-23 02:39:21 +0300

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Abdelrahman Mohamed on 23.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
